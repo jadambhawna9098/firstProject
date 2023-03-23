1443190480bd13bf901a39f502727d4e9ddec3fd
jadambhawna9098 <128667446+jadambhawna9098@users.noreply.github.com>
2023-03-23 10:04:18 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void main()
+{
+ printf("\t\t\t\t\tDAVV INDORE");
+ printf("\nRno   :12345\t\t\t\t\t\t\t");
+ printf("Course :C,C++");
+ printf("\nName  :Bhawna Rajput\t\t\t\t\t\t");
+ printf("Branch :Indore");
+ printf("\nFname :Pratap\t\t\t\t\t\t\t");
+ printf("Sem    :Final");
+ printf("\nMName :Singh\t\t\t\t\t\t\t");
+ printf("Year   :third\n\n\n");
+ printf("\tSubCode   ");
+ printf("\tSubName   ");
+ printf("\tMaxMarks");
+ printf("\tMinMarks");
+ printf("\tObtMarks\n\n");
+ printf("\tCSE-501   ");
+ printf("\t-------   ");
+ printf("\t  70");
+ printf("\t\t   21");
+ printf("\t\t  --\n\n");
+ printf("\tCSE-502   ");
+ printf("\t-------   ");
+ printf("\t  70");
+ printf("\t\t   21");
+ printf("\t\t  --\n\n");
+ printf("\tCSE-503   ");
+ printf("\t-------   ");
+ printf("\t  70");
+ printf("\t\t   21");
+ printf("\t\t  --\n\n");
+ printf("\tCSE-504   ");
+ printf("\t-------   ");
+ printf("\t  70");
+ printf("\t\t   21");
+ printf("\t\t  --\n\n");
+ printf("\tCSE-505   ");
+ printf("\t-------   ");
+ printf("\t  70");
+ printf("\t\t   21");
+ printf("\t\t  --\n\n");
+ printf("\ttotal      :105/350");
+ printf("\n--------------------------------------------------------------------------------------------");
+}
